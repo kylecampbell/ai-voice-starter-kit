a6d1f50b0245ac2aceac7b846325a16807c781c8
kyle <kylejaycampbell@gmail.com>
2024-06-01 12:27:37 +1000

    first commit

---
+++ open_ai.py
@@ -0,0 +1,22 @@
+import os
+from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+client = OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
+
+def get_gpt_response(prompt):
+    """OpenAI Chat Completion"""
+    response = client.chat.completions.create(
+        messages=[
+            {
+                "role": "user",
+                "content": prompt,
+            }
+        ],
+        model="gpt-3.5-turbo",
+    )
+    return response.choices[0].message.content.strip()
